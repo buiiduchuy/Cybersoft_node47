fc178f7e2cb7a7ab1d82bcab77ba2b304ee9fe1d
HuyVN <buiiduchuy@gmail.com>
2024-10-03 21:42:32 +0700

    push code session 5

---
+++ db.js
@@ -0,0 +1,16 @@
+// tạo object để kết nối với database
+import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
+
+// load biến môi trường từ file .env
+dotenv.config()
+
+const connect = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+})
+
+export default connect
---
+++ index.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import connect from './db.js';
+
+// tạo object tổng của express
+const app = express();
+
+// Thêm middlware để convert string về json với API POST và PUT
+app.use(express.json());
+
+// viết API hello word
+app.get("/hello-world",(req,res) => {
+  res.send("hello-world");
+})
+
+app.get("/health-check",(req,res) => {
+  res.send("Server is normally")
+})
+
+// lấy thông tin data từ params , query string , headers , body
+// http://localhost:8080/get-user/1
+app.get("/get-user/:id/:hoTen",(req,res)=> {
+  // lấy id từ url
+  let {id,hoTen} = req.params;
+  let {queryString} = req.query
+  let {token,authorization} = req.headers
+  let headers = req.headers
+  res.send({id,hoTen,queryString,token,authorization});
+})
+
+app.get("/get-user-db", async (req,res) => {
+  const [data] = await connect.query(`
+      SELECT * FROM users
+    `)
+    res.send(data);
+})
+
+// lấy body từ API POST (create) và PUT (Update)
+'{ "id": 1,"hoTen": "Halo"}'
+app.post("/create-user",(req,res)=> {
+  let body = req.body
+  res.send(body)
+})
+
+
+app.post("/create-user-db" , async (req,res)=> {
+  const query = `
+    INSERT INTO users(full_name,email,pass_word) VALUES
+    (?,?,?)
+  `
+  let body = req.body;
+  let {full_name,email,pass_word} = body
+  const [data] = await connect.execute(query, [full_name,email,pass_word])
+  res.send(data)
+})
+
+// define port for BE
+app.listen(8080,() => {
+  console.log('BE starting with port 8080');
+})
